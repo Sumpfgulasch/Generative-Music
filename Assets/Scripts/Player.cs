c81b00a92a6f20ac4369ecca76d7accab88ca71b
Julian <kontakt@julianhellmann.de>
2020-10-28 15:08:14 +0100

    neue Maus-Steuerung; Keyboard-Steuerung; simples Feedback für Maus

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,48 +4,165 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // public variables
+    [Header("Mouse")]
     public float rotationSpeed = 5f;
     public float scaleSpeed = 5f;
-    public float maxScale = 10f;
+    public float scaleMax = 2.7f;
+    public float scaleMin = 1f;
+    public float scaleSensitivity = 0.3f;
 
-    // Start is called before the first frame update
+    [Header("Keyboard")]
+    public float kb_scaleSpeed = 1f;
+    public float kb_rotationSpeed = 1f;
+
+    // private variables
+    private MeshRenderer meshRenderer;
+    private Color defaultColor;
+    private Color moveColor;
+
+    private enum MouseState {hover, move, none};
+    MouseState mouseState = MouseState.none;
+    private bool mouseIsActive;
+    private Vector3 mouseStartPos;
+    private Vector3 mousePos;
+    private Vector3 lastMousePos = Vector3.zero;
+    private Vector3 midPoint;
+    private float rotationTargetAngle;
+    private float scaleTargetValue;
+
+
     void Start()
     {
+        meshRenderer = this.transform.GetComponentInChildren<MeshRenderer>();
+        defaultColor = meshRenderer.material.color;
+        moveColor = defaultColor * 1.2f;
+        midPoint = this.transform.position;
+    }
+
+
+    void Update()
+    {
+        GetInput();
+        ManageMovement();
+    }
+
+
+    void ManageMovement()
+    {
+        MouseMovement();
+        KeyboardMovement();
+    }
+
+
+    void KeyboardMovement ()
+    {
+        // rotation
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        if (horizontalAxis != 0)
+        {
+            this.transform.eulerAngles -= new Vector3(0, 0, horizontalAxis * kb_rotationSpeed);
+        }
+
+
+        // scale
+        float verticalAxis = Input.GetAxis("Vertical");
+        if (verticalAxis != 0)
+        {
+            this.transform.localScale += new Vector3(verticalAxis * kb_scaleSpeed, verticalAxis * kb_scaleSpeed, 0);
+        }
+        this.transform.localScale = ClampVector3(this.transform.localScale, scaleMin, scaleMax);
+    }
+    
+
+    void MouseMovement()
+    {
+        if (mouseState == MouseState.hover)
+        {
+            meshRenderer.material.color = Color.white;
+        }
+
+        else if (mouseState == MouseState.move)
+        {
+            meshRenderer.material.color = moveColor;
+
+            // rotation
+            Vector2 mouseToMid = mousePos - midPoint;
+            Vector2 lastMouseToMid = lastMousePos - midPoint;
+            rotationTargetAngle = Vector2.SignedAngle(lastMouseToMid, mouseToMid);
+            this.transform.eulerAngles += new Vector3(0, 0, rotationTargetAngle);
+
+            // scale
+            scaleTargetValue = (mouseToMid.magnitude - lastMouseToMid.magnitude) * scaleSensitivity;
+            this.transform.localScale += new Vector3(scaleTargetValue, scaleTargetValue, 0);
+
+            lastMousePos = mousePos;
+
+        }
+
+        else
+        {
+            meshRenderer.material.color = defaultColor;
+        }
+    }
+
+    //public Vector2 FindNearestPointOnLine(Vector2 origin, Vector2 end, Vector2 point)
+    //{
+    //    //Get heading
+    //    Vector2 heading = (end - origin);
+    //    float magnitudeMax = heading.magnitude;
+    //    heading.Normalize();
+
+    //    //Do projection from the point but clamp it
+    //    Vector2 lhs = point - origin;
+    //    float dotP = Vector2.Dot(lhs, heading);
+    //    dotP = Mathf.Clamp(dotP, 0f, magnitudeMax);
+    //    return origin + heading * dotP;
+    //}
+
+        void GetInput()
+    {
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseState = MouseState.move;
+            lastMousePos = mousePos;
+            //mouseStartPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+            //mouseStartPos = Camera.main.ScreenToWorldPoint(mouseStartPos);
+        }
+        else if (!Input.GetMouseButton(0))
+            mouseState = MouseState.hover;
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseExit()
     {
-        Scale();
-        Rotate();
+        if (mouseState != MouseState.move)
+            mouseState = MouseState.none;
     }
 
-    void Scale()
+    void OnMouseUp()
     {
-        // Mouse
-        float mouseX = Input.GetAxis("Mouse X");
-        this.transform.localScale += new Vector3(mouseX, mouseX, 0) * scaleSpeed;
-        this.transform.localScale = new Vector3(Mathf.Clamp(this.transform.localScale.x, 0, maxScale), Mathf.Clamp(this.transform.localScale.y, 0, maxScale), this.transform.localScale.z);
+        mouseState = MouseState.none;
     }
 
-    void Rotate()
-    {
-        // Mouse
-        this.transform.eulerAngles += new Vector3(0, 0, Input.GetAxis("Mouse Y") * rotationSpeed);
-    }
 
-    public Vector2 FindNearestPointOnLine(Vector2 origin, Vector2 end, Vector2 point)
-    {
-        //Get heading
-        Vector2 heading = (end - origin);
-        float magnitudeMax = heading.magnitude;
-        heading.Normalize();
 
-        //Do projection from the point but clamp it
-        Vector2 lhs = point - origin;
-        float dotP = Vector2.Dot(lhs, heading);
-        dotP = Mathf.Clamp(dotP, 0f, magnitudeMax);
-        return origin + heading * dotP;
+    // Extension Methods
+
+    Vector3 ClampVector3(Vector3 value, float min, float max)
+    {
+        Vector3 clampedVector3;
+        clampedVector3 = new Vector3(
+            Mathf.Clamp(value.x, min, max),
+            Mathf.Clamp(value.y, min, max),
+            value.z);
+        return clampedVector3;
     }
 }
