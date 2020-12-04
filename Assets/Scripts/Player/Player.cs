4397f386d3e57d60364d3dba074d3528da71aef9
Julian <kontakt@julianhellmann.de>
2020-12-04 14:54:04 +0100

    Revert "Vertex-Zählrichtung geändert; auch szene geändert; Maus Werte falsch"
    This reverts commit 28576eab1ba5b7c12a58c33e711390a642937a0f.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -158,7 +158,6 @@ public partial class Player : MonoBehaviour
         curPlayerRot = Mathf.Clamp(curPlayerRot, -rotationMaxSpeed, rotationMaxSpeed); // = max speed
         rotTargetValue = rotationTargetVectorFactor * curPlayerRot;
         curRotSpeed = rotTargetValue;
-        Debug.DrawLine(mousePos, midPoint, Color.green);
 
         // SCALE
         // Distances
